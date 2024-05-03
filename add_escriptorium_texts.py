f6d793a12288326d57910a563026b5218c8b34d1
pverkind <peter.verkinderen@gmail.com>
2024-05-03 11:52:54 +0200

    Move new escriptorium text to corpus

--- add_escriptorium_texts.py
+++ add_escriptorium_texts.py
@@ -1250,6 +1250,7 @@ if __name__ == "__main__":
     meta_fp = "meta/OCR_URIs_2022_2023 - ESCRIPTORIUM (6).tsv"
     meta_fp = "meta/OCR_URIs_2022_2023 - ESCRIPTORIUM (7).tsv"
     meta_fp = "meta/OCR_URIs_2022_2023 - ESCRIPTORIUM (8).tsv"
+    meta_fp = "meta/OCR_URIs_2022_2023 - ESCRIPTORIUM (9).tsv"
     
 
 
@@ -1257,6 +1258,6 @@ if __name__ == "__main__":
     dest_folder = "."
     add_eScriptorium_files(meta_fp, download_folder, dest_folder,
                            reconvert=True,              # convert even if the file is already in the corpus or barzakh
-                           first_row=140, last_row=141,   # from row X to row Y in the metadata spreadsheet
+                           first_row=183, last_row=183,   # from row X to row Y in the metadata spreadsheet
                            redownload=True             # even if the transcription was already downloaded
                            )
